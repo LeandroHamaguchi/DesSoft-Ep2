7d65c05f5834f53cb0432e210ec7a443fbb0ec52
LeandroHamaguchi <brasil.leandrohv@gmail.com>
2023-04-19 16:44:34 -0300

    função define_posicoes implementada

---
+++ define_posicoes.py
@@ -0,0 +1,11 @@
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    i = 0
+    posicao_navio = [0]*tamanho
+    while i < tamanho:
+        if orientacao == "vertical":
+            posicao_navio[i] = [linha + i, coluna]
+        else:
+            posicao_navio[i] = [linha, coluna + i]
+        i+=1
+        
+    return posicao_navio
