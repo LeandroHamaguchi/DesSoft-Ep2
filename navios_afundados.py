98534b3611cdbf44be012aa4ed3f8d0264f40b1d
LeandroHamaguchi <brasil.leandrohv@gmail.com>
2023-04-24 15:20:50 -0300

    implementando navios_afundados e posicao_valida

---
+++ navios_afundados.py
@@ -0,0 +1,18 @@
+def afundados(frota, tabuleiro):
+    lista_posicoes = []
+    lista_x = 0
+    afundados = 0
+
+    for navio in frota.values():
+        for i in range(len(navio)):
+            lista_posicoes.append(navio[i])
+
+    for navios in lista_posicoes:
+        lista_x = 0
+        for posicoes in navios:
+            if tabuleiro[posicoes[0]][posicoes[1]] == 'X':
+                lista_x += 1
+                if lista_x == len(navios):
+                    afundados += 1
+    
+    return afundados
